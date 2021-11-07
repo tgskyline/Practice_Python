e8ffd8d06eedd4b05b3d52a819826307ccd785dd
tgskyline <tigomes.santos@gmail.com>
2021-11-07 17:59:22 -0300

    PythonExercise_III

--- PythonExercise_III.py
+++ PythonExercise_III.py
@@ -1,3 +1,15 @@
+'''
+Escreva um programa em Python para calcular a popularidade
+das linguagens de programação em uma escola de informática.
+O programa deve receber do teclado o nome da pessoal e a
+linguagem de programação que ela programa. O programa
+deve fornecer um menu de opção entras as três linguagens
+ensinadas no curso: (1) C++; (2) Java; e (3) Python.
+Crie um dicionário para armazenar cada pessoa e o valor de
+seu voto. Ao final, imprima quantos votos que cada linguagem
+obteve.
+'''
+
 linguagens = {}
 
 for i in range (0, 3):
