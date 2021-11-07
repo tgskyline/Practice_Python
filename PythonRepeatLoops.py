e6407a028dc9d063f33a683d426319b80d5b1428
tgskyline <tigomes.santos@gmail.com>
2021-11-07 10:05:23 -0300

    PythonReapeatLoops

---
+++ PythonRepeatLoops.py
@@ -0,0 +1,11 @@
+materiais =['lápis', 'caderno', 'borracha', 'caneta']
+print(materiais)
+
+for item in materiais:
+  print(item)
+  print(item.title())
+
+print(materiais)
+
+for valor in range(1,5):
+  print(valor)
