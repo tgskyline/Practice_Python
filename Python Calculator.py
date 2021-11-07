b9a235b44fc09bec2c129b12327d893b1b0835b0
tgskyline <tigomes.santos@gmail.com>
2021-11-07 09:43:27 -0300

    PythonCalculator

---
+++ Python Calculator.py
@@ -0,0 +1,16 @@
+import math
+
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+
+soma = a + b
+sub = a - b
+mult = a + b
+div = a / b
+raiz = math.sqrt(a)
+
+print("soma: " + str(soma))
+print("sub: " + str(sub))
+print("mult: " + str(mult))
+print("div: " + str(div))
+print("raiz: " + str(raiz))
