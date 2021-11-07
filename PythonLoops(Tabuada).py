fe510db7b56c6dc1a3195562969de63a6bfb95da
tgskyline <tigomes.santos@gmail.com>
2021-11-07 10:09:33 -0300

    PythonLoops_Tabuada

---
+++ PythonLoops(Tabuada).py
@@ -0,0 +1,8 @@
+
+# Tabuada, exemplo de laço do laço rs
+
+for i in range(1,11):
+  print("Tabuada do " + str (i))
+  for j in range(1,11):
+    r=j*i
+    print(str(i) + " x "+ str(j)+ " = "+ str(r))
