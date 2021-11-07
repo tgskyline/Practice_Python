fb09309bf4ff490ebe88b0383ffea82df023e427
tgskyline <tigomes.santos@gmail.com>
2021-11-07 10:06:52 -0300

    PythonNumberList

--- PythonNumberList.py
+++ PythonNumberList.py
@@ -1,5 +1,30 @@
 materiais =['lápis', 'caderno', 'borracha', 'caneta']
 print(materiais)
 
-numeros = list(range(1,6))
+# Lista de 1 a 10
+numeros = list(range(1,11))
 print(numeros)
+
+for valor in numeros:
+  print(valor)
+
+  quadrados = []
+
+for item in range(1,11):
+  quadrado = item ** 2
+  quadrados.append(quadrado)
+
+print(quadrados)
+
+print(min(numeros))
+print(max(numeros))
+print(sum(numeros))
+
+print(materiais[0:3])
+
+mat = [1,2,3,4]
+# Quando utilizo a atribuição de variável com [:] estou fazendo uma cópia da lista
+obj = mat[:] 
+obj.append(5)
+print(mat)
+print(obj)
