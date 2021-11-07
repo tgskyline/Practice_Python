646f92ae0ea54db9a5225f575955e5f0676b4cdd
tgskyline <tigomes.santos@gmail.com>
2021-11-07 09:37:58 -0300

    Parameters

---
+++ Initial Python.py
@@ -0,0 +1,77 @@
+# mensagem = "Hello World With VAR"
+
+# print(mensagem)
+
+# num1 = 10
+# print("O primeiro níumero é: " + str(num1))
+
+# num2 = 20
+# print("O primeiro níumero é: " + str(num2))
+
+# nome = "Python"
+
+# print(nome.upper())
+# print(nome.lower())
+
+# Operações 
+# + = soma
+# - = subtração
+# / = divisão
+# ** = pontenciação
+
+# preco = 20
+# a = 10
+# b = 2
+# c = 5
+
+# d = a + b * c + a
+# print(d)
+
+# nome = input("Digite seu nome: ")
+# print("Olá " + nome)
+
+# # a = int(input("Digite o primeiro número: "))
+# # b = int(input("Digite o segundo número: "))
+
+# a = float(input("Digite o primeiro número: "))
+# b = float(input("Digite o segundo número: "))
+
+# print("Resultado da soma: "+ str(a+b))
+
+# num1 = 10
+# print ("O primeiro número é: " + num1)
+
+# LISTAS
+
+# O último elemento da lista é acessado com -1
+
+materiais = ['caneta','caderno','livro','lapis']
+# materiais[2]='e-book'
+
+numeros =[4,10,8,6,5,1,7,2,3]
+
+materiais.append('lista') 
+
+# Append inclui um termo na lista da variável
+
+materiais.insert(0,'borracha')
+
+# Insert insere um novo termo e sua posição
+
+# del materiais[0]
+
+# del exclui um item da lista
+
+# elemento = materiais.pop()
+
+# .pop retira o último termo da lista, mas o mantém armazenado
+
+materiais.sort()
+
+numeros.sort()
+print(numeros)
+
+
+# print(elemento)
+print(materiais)
+
